8fcadb612efd885770ccf79c5bf019561a6a3d1f
root <root@localhost.localdomain>
2018-09-26 15:54:35 +0300

    test class for Image,Location,Category

--- snaps/models.py
+++ snaps/models.py
@@ -5,7 +5,9 @@ import datetime as dt
 class Image(models):
     image = models.ImageField(upload_to = 'category/', default='DEFAULT IMAGE')
     image_name = models.TextField()
-    description = models.TextField()
+    image_description = models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
 
     def save_image(self):
         self.save()
@@ -16,5 +18,24 @@ class Image(models):
     def update_image(self):
         self.update()
     
+    def search_by_category(cls,search_term):
+        snaps = cls.object.filter(image_icontains=search_term)
     
+    def filter_by_location(cls,filter_term):
+        snaps = cls.object.filter(image_icontains=filter_term)
+    
+class Location(models.Model):
+    location =  models.TextField()
+
+    def __str__(self):
+        return self.location
+
+    def __str__(self):
+        return self.location
+
+class Meta:
+    ordering = ['location']
+
+class Category(models.Model):
+    category = models.TextField(Image)
 
