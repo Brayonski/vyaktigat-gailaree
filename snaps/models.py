35fae76235bc44d84bb0caf99fb5a0923d7dc3b6
root <root@localhost.localdomain>
2018-09-26 15:25:35 +0300

    save,delete & update models

--- snaps/models.py
+++ snaps/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+import datetime as dt
 # Create your models here.
+
+class Image(models):
+    image = models.ImageField(upload_to = 'category/', default='DEFAULT IMAGE')
+    image_name = models.TextField()
+    description = models.TextField()
+
+    def save_image(self):
+        self.save()
+    
+    def delete_image(self):
+        self.delete()
+    
+    def update_image(self):
+        self.update()
+    
+    
+
