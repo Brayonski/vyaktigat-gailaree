451cc622dac6cfcbc1d22948bacf212cecbb1309
root <root@localhost.localdomain>
2018-09-26 16:53:19 +0300

    urlpattern for search and snaps

--- snaps/urls.py
+++ snaps/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns=[
     url('$',views.welcome,name='welcome'),
+    url('^###/$',views.wholegallery,name='allsnaps'),
+    url('^search/',views.search_results, name='search_results')
 ]
