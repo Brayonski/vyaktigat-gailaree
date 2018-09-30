49e1b5dbf3a330f7c91f0320f4b8f8cae9b547b5
root <root@localhost.localdomain>
2018-09-30 21:34:28 +0300

    rendering pictures to snaps.html

--- snaps/views.py
+++ snaps/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 import datetime as dt
+from .models import Image
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('welcome to snapy snapy')
+    return render(request,'welcome.html')
 
+def wholegallery(request):
+    snaps = Image.objects.all()
+    return render(request, 'all-snaps/snaps.html',{"snaps":snaps})
 
+def search_results(request):
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_categories = Category.search_by_image(search_term)
+        message = f"{search_term}"
+        return render(request, 'all-snaps/search.html', {"message":message, "categories": searched_categories})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-snaps/search.html',{"message":message},)
+
+def image(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-snaps/snaps.html", {"image":image})
