3379257d4339ef780607e6d283b9398fa662c113
root <root@localhost.localdomain>
2018-09-30 21:29:54 +0300

    search functionality implementation

--- snaps/models.py
+++ snaps/models.py
@@ -1,14 +1,35 @@
 from django.db import models
 import datetime as dt
 # Create your models here.
+class Location(models.Model):
+    location =  models.CharField(max_length=50)
 
-class Image(models):
-    image = models.ImageField(upload_to = 'category/', default='DEFAULT IMAGE')
-    image_name = models.TextField()
+    def __str__(self):
+        return self.location
+
+    class Meta:
+        ordering = ['location']
+
+class Category(models.Model):
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to= 'media/')
+    image_name = models.CharField(max_length=50)
     image_description = models.TextField()
     location = models.ForeignKey(Location)
     category = models.ForeignKey(Category)
 
+    @classmethod
+    def all_snaps(cls):
+        today = dt.date.today()
+        snaps = cls.objects.filter(pub_date_date = today)
+        return snaps
+        
     def save_image(self):
         self.save()
     
@@ -18,24 +39,11 @@ class Image(models):
     def update_image(self):
         self.update()
     
-    def search_by_category(cls,search_term):
+    def search_by_image(cls,search_term):
         snaps = cls.object.filter(image_icontains=search_term)
     
     def filter_by_location(cls,filter_term):
         snaps = cls.object.filter(image_icontains=filter_term)
+
     
-class Location(models.Model):
-    location =  models.TextField()
-
-    def __str__(self):
-        return self.location
-
-    def __str__(self):
-        return self.location
-
-class Meta:
-    ordering = ['location']
-
-class Category(models.Model):
-    category = models.TextField(Image)
-
+    
