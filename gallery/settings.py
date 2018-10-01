83438d6ad4e9adb9958174cecd6b29c0dd70d36e
root <root@localhost.localdomain>
2018-10-01 08:22:05 +0300

    deploy

--- gallery/settings.py
+++ gallery/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import django_heroku
 import dj_database_url
+from decouple import config,Csv
+
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+# development
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,8 +33,7 @@ SECRET_KEY = '(n1x)tpy+psu&0s$$*snvs1l86(s_=%a#6gv7vq-v$j$d2+x)s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
@@ -81,36 +86,37 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 
-# if config('MODE')=="dev":
-#       DATABASES = {
-#       'default': {
-#           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#           'NAME': config('DB_NAME'),
-#           'USER': config('DB_USER'),
-#           'PASSWORD': config('DB_PASSWORD'),
-#           'HOST': config('DB_HOST'),
-#           'PORT': '',
-#     }
-
-#   }
-# else:
-#    DATABASES = {
-#        'default': dj_database_url.config(
-#            default=config('DATABASE_URL')
-#            )
-#     }
-
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gallery',
-        'USER': 'root',
-        'PASSWORD': 'mutaikips',
+if config('MODE')=="dev":
+      DATABASES = {
+      'default': {
+          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+          'NAME': config('DB_NAME'),
+          'USER': config('DB_USER'),
+          'PASSWORD': config('DB_PASSWORD'),
+          'HOST': config('DB_HOST'),
+          'PORT': '',
     }
-}
 
+  }
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+           )
+    }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'gallery',
+#         'USER': 'root',
+#         'PASSWORD': 'mutaikips',
+#     }
+# }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -164,3 +170,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+# Configure Django app for heroku
+django_heroku.settings(locals())
